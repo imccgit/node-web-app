0a0b9c1daa71b4f0408822eb45cb5d1da460cfe3
imccgit <ian.mccallum.1@gmail.com>
2017-05-09 23:49:10 +0200

    Delete build.sh

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-DOCKER_FILE=${1:-Dockerfile}
-DOCKER_IMAGE_NAME=node/node-web-app
-DOCKER_IMAGE_TAG=1.0
-
-echo "Building Docker image: ${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG}"
-docker build -t ${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG}.
-
