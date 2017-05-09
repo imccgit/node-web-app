635bf26f7c85ac8c12204de8c8eed26cffdd56b9
imccgit <ian.mccallum.1@gmail.com>
2017-05-09 23:41:02 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,10 +4,5 @@ DOCKER_IMAGE_NAME=node/node-web-app
 DOCKER_IMAGE_TAG=1.0
 
 echo "Building Docker image: ${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG}"
-docker build -t ${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG} -f ${DOCKER_FILE} .
-if [ $? -ne 0 ]; then
-  echo "Failed to build image: ${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG}"
-  exit 1
-fi 
+docker build -t ${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG}.
 
-docker tag -f ${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG} ${DOCKER_IMAGE_NAME}
